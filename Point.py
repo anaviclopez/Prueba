3266193df05bcadcdf87a033582c862019fa1297
anaviclopez <73209494+anaviclopez@users.noreply.github.com>
2020-12-02 20:43:14 -0600

    Delete Point.py

--- Point.py
+++
@@ -1,9 +0,0 @@
-class Point(object):
-
-    def __init__(self, x, y):
-
-        self.x = x
-        self.y = y
-
-    def ImprimirPunto(self):
-        return print('Punto[x = '+ x +',y = '+ y + ']')
