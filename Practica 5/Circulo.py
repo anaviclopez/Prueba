ab8ab0dfbc2d9ea015f4ca4d75a7cc3ecd71ca55
DESKTOP-IPGHHVM\anaviclopez <alopez@devblocks.io>
2021-01-04 13:16:46 -0600

    04/01

---
+++ Practica 5/Circulo.py
@@ -0,0 +1,10 @@
+class Circulo():
+    PI = 3.14159
+    def __init__(self,radio):
+        self.__radio = radio
+    def get_radio(self): 
+        return self.__radio
+    def set_radio(self, ):
+        self.__radio = radio
+    def perimetro(self):
+        return 2*PI*__radio
