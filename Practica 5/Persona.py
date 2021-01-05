4b2bf30fe1c4a87a9629630c1b4d26c63c9fa5ed
DESKTOP-IPGHHVM\anaviclopez <alopez@devblocks.io>
2021-01-05 12:43:26 -0600

    05/01 12:43

--- Practica 5/Persona.py
+++ Practica 5/Persona.py
@@ -1,8 +1,9 @@
-
+from Fecha import Fecha
 class Persona():
-    def __init__(self,nombre,apellido):
+    def __init__(self,nombre,apellido,fnacimiento):
         self.__nombre = nombre
         self.__apellido = apellido
+        self.__fnacimiento = fnacimiento
 
     @property
     def nombre(self):
@@ -12,7 +13,6 @@ class Persona():
     def nombre(self, value):
         self.__nombre = value
 
-
     @property
     def apellido(self):
         return self.__apellido
@@ -20,4 +20,11 @@ class Persona():
     @apellido.setter
     def apellido(self, value):
         self.__apellido = value
+# La composición es una forma de reutilización de software, en donde una clase tiene como miembros referencias a objetos de otras clases
+    @property
+    def fnacimiento(self):
+        return self.__fnacimiento
 
+    @fnacimiento.setter
+    def fnacimiento(self,value):
+        self.__fnacimiento = Fecha(value.dia,value.mes,value.anio)
--- Practica 5/PruebaPersona.py
+++ Practica 5/PruebaPersona.py
@@ -3,16 +3,21 @@ from Persona import Persona
 
 class PruebaPersona():
     def __init__(self):
-        Per1 = Persona(None,None)
+        Per1 = Persona(None,None,None)
         Nac = Fecha(None,None,None)
         Per1.nombre = 'Juan'
         Per1.apellido = 'Perez'
         Nac.dia = 15
         Nac.mes = 11
-        Nac.anio = 1950
+        Nac.anio = 1970
+        Per1.fnacimiento = Nac
         print('Nombre: ' + Per1.nombre)
         print('Apellido: ' + Per1.apellido)
-        print('Fecha de Nacimiento:' + str(Nac.dia) +'/' + str(Nac.mes) +'/' + str(Nac.anio) )
-
+        print('Fecha de Nacimiento:' + str(Per1.fnacimiento.dia) +'/' + str(Per1.fnacimiento.mes) +'/' + str(Per1.fnacimiento.anio) )
+        
+        #Cambio de datos
+        Nac.anio = 2000
+        Per1.fnacimiento = Nac
+        print('Fecha de Nacimiento:' + str(Per1.fnacimiento.dia) +'/' + str(Per1.fnacimiento.mes) +'/' + str(Per1.fnacimiento.anio) )
 
 PruebaPersona()
